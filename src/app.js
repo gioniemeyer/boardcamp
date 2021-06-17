924268f190cbf200eee84b9eb6aa876dd5a701f5
Giovanna Niemeyer <giovanna.niemeyer@gmail.com>
2021-06-17 19:24:53 -0300

    inclusão de clientes na tabela customers e visualização pela rota get

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/categories', async (req, res) => {
-    const response = await connection.query('select * from categories')
+    const response = await connection.query('SELECT * FROM categories')
     res.send(response.rows);
 })
 
@@ -35,23 +35,23 @@ app.post('/categories', async (req, res) => {
     return res.sendStatus(400);
   }
 
-  const checkName = await connection.query('select * from categories where name ilike $1',[name])
+  const checkName = await connection.query('SELECT * FROM categories WHERE name iLIKE $1',[name])
   if(checkName.rows[0]) return res.sendStatus(409) 
 
-  await connection.query('insert into categories ("name") values ($1)',[name]);
+  await connection.query('INSERT INTO categories ("name") VALUES ($1)',[name]);
   return res.sendStatus(201);
 })
 
 // ---
 
 app.get('/games', async (req, res) => {
-  const { title } = req.query;
+  const { name } = req.query;
   let responseGame = '';
 
-  if(title) {
-    responseGame = await connection.query('select * from games where name ilike $1',[title + '%']);
+  if(name) {
+    responseGame = await connection.query('SELECT * FROM games WHERE name iLIKE $1',[name + '%']);
   } else {
-    responseGame = await connection.query('select * from games');
+    responseGame = await connection.query('SELECT * FROM games');
   }
   
   res.send(responseGame.rows);
@@ -61,16 +61,48 @@ app.get('/games', async (req, res) => {
 app.post('/games', async (req, res) => {
   const {name, image, stockTotal, categoryId, pricePerDay} = req.body;
 
-  const checkCategoryId = await connection.query('select * from categories where id = $1',[categoryId]);
+  const checkCategoryId = await connection.query('SELECT * FROM categories WHERE id = $1',[categoryId]);
 
   if(!name || name.length === 0 || !stockTotal || stockTotal === 0 || !pricePerDay || pricePerDay === 0 || checkCategoryId.rows.length === 0) {
     return res.sendStatus(400);
   }
 
-  const checkName = await connection.query('select * from games where name ilike $1',[name])
+  const checkName = await connection.query('SELECT * FROM games WHERE name iLIKE $1',[name])
   if(checkName.rows[0]) return res.sendStatus(409);
 
-  await connection.query('insert into games ("name", "image", "stockTotal", "categoryId", "pricePerDay") values ($1, $2, $3, $4, $5)',[name, image, stockTotal, categoryId, pricePerDay]);
+  await connection.query('INSERT INTO games (name, image, "stockTotal", "categoryId", "pricePerDay") VALUES ($1, $2, $3, $4, $5)',[name, image, stockTotal, categoryId, pricePerDay]);
+  return res.sendStatus(201);
+
+})
+
+//----
+
+app.get('/customers', async (req, res) => {
+  const { cpf } = req.query;
+  let responseCustomers = '';
+
+  if(cpf) {
+    responseCustomers = await connection.query(`SELECT * FROM customers WHERE cpf iLIKE $1`, [cpf + '%']);
+  } else {
+    responseCustomers = await connection.query('SELECT * FROM customers');
+  }
+  
+  res.send(responseCustomers.rows);
+
+})
+
+app.post('/customers', async (req, res) => {
+  const {name, phone, cpf, birthday} = req.body;
+
+  if(!name || name.length === 0 || !(9999999999 < cpf < 99999999999) || !(999999999 < phone < 99999999999) || !birthday || !Date.parse(birthday) || Date.parse(birthday) > Date.now()) {
+    return res.sendStatus(400);
+  }
+
+  const checkCpf = await connection.query('SELECT * FROM customers WHERE cpf iLIKE $1',[cpf])
+  if(checkCpf.rows[0]) return res.sendStatus(409);
+
+  await connection.query('insert into customers (name, phone, cpf, birthday) values ($1, $2, $3, $4)',[name, phone, cpf, birthday]);
+  console.log(birthday)
   return res.sendStatus(201);
 
 })
